3ba9564038c5c0b32adbf8236d19d2557b4adfdf
Omar Vásquez Machorro <zS24016731@estudiantes.uv.mx>
2026-02-20 15:22:51 -0600

    Delete PairProgramingExceptions-main directory

--- PairProgramingExceptions-main/src/Ejercicio_5/NumeroNegativoException.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio_5;
-
-public class NumeroNegativoException extends Exception {  //la excepción personalizada es para manejar casos específicos de números negativos
-
-        public NumeroNegativoException(String mensaje) {
-            super(mensaje);
-        }
-    }
-

87278680a896987c12ad2027efab665663f328f8
Omar Vásquez Machorro <zS24016731@estudiantes.uv.mx>
2026-02-20 15:24:03 -0600

    Delete src directory

--- src/Ejercicio3_4/NumeroNegativoException.java
+++
@@ -1,7 +0,0 @@
-package Ejercicio3_4;
-public class NumeroNegativoException extends Exception {  //la excepción personalizada es para manejar casos específicos de números negativos
-
-    public NumeroNegativoException(String mensaje) {
-        super(mensaje);
-    }
-}
